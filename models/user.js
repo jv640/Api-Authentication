8661ff7bdfaec035c7f9e3a384f2ecfffcc456b1
jv640 <vermajatin640@gmail.com>
2020-06-28 02:09:03 +0530

    comment

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const { schemas } = require('../helpers/routehelpers');
-const { string } = require('@hapi/joi');
+// const { schemas } = require('../helpers/routehelpers');
+// const { string } = require('@hapi/joi');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
 
